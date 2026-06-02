ce46d5ca12678fd58713c2759780988c038bc0c5
Renato Siqueira <renato@Renatos-MacBook-Air.local>
2026-06-01 21:42:20 -0500

    health redis

--- app/routes/health.py
+++ app/routes/health.py
@@ -9,6 +9,10 @@ from app.database.session import (
     SessionLocal
 )
 
+from app.infrastructure.cache.cache_service import (
+    ping_cache
+)
+
 router = APIRouter(
     prefix="/health",
     tags=["Health"]
@@ -61,8 +65,47 @@ def readiness_check():
     "",
     status_code=status.HTTP_200_OK
 )
-def health_check():
+async def health_check():
+
+    redis_connected = await ping_cache()
+
+    db = SessionLocal()
+
+    try:
+
+        db.execute(
+            text("SELECT 1")
+        )
+
+        database_connected = True
+
+    except Exception:
+
+        database_connected = False
+
+    finally:
+
+        db.close()
+
+    overall_status = (
+        "healthy"
+        if (
+            redis_connected
+            and database_connected
+        )
+        else "degraded"
+    )
 
     return {
-        "status": "healthy"
+        "status": overall_status,
+        "database": (
+            "up"
+            if database_connected
+            else "down"
+        ),
+        "redis": (
+            "up"
+            if redis_connected
+            else "down"
+        )
     }
